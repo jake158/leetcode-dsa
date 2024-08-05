8217e552b35d90dfb0d2678a33011bd2d5beadd1
freeram <kulikovpavel2801@gmail.com>
2024-08-05 13:46:06 -0600

    Add linked list ADT

---
+++ DataStructures/linked-list.js
@@ -0,0 +1,43 @@
+class Node {
+  constructor() {
+    this.value = null;
+    this.nextNode = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
+
+  append(value) {}
+
+  prepend(value) {}
+
+  at(index) {}
+
+  insertAt(value, index) {}
+
+  removeAt(index) {}
+
+  pop() {}
+
+  contains(value) {}
+
+  find(value) {}
+
+  toString() {}
+}
+
+// const list = new LinkedList();
+
+// list.append("dog");
+// list.append("cat");
+// list.append("parrot");
+// list.append("hamster");
+// list.append("snake");
+// list.append("turtle");
+
+// console.log(list.toString());
