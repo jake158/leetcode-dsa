07b8d6297a2fffe542a073c56cc80a84ed7e2c41
freeram <kulikovpavel2801@gmail.com>
2024-08-05 19:39:21 -0600

    Implement set

--- DataStructures/hashmap.js
+++ DataStructures/hashmap.js
@@ -1,5 +1,14 @@
 class HashMap {
-  constructor() {}
+  constructor() {
+    this.buckets = [];
+    this.size = 16;
+    this.loadFactor = 0.75;
+
+    for (let i = 0; i < this.size; i++) {
+      // Let's pretend these are linked lists
+      this.buckets.push([]);
+    }
+  }
 
   hash(key) {
     let hashCode = 0;
@@ -10,7 +19,17 @@ class HashMap {
     return hashCode;
   }
 
-  set(key, value) {}
+  set(key, value) {
+    const index = this.hash(key) % this.size;
+    const bucket = this.buckets[index];
+    const existingEl = bucket.find((el) => el.key === key);
+
+    if (existingEl) {
+      existingEl.value = value;
+    } else {
+      bucket.push({ key, value });
+    }
+  }
 
   get(key) {}
 
@@ -28,3 +47,18 @@ class HashMap {
 
   entries() {}
 }
+
+const test = new HashMap();
+
+test.set('apple', 'red');
+test.set('banana', 'yellow');
+test.set('carrot', 'orange');
+test.set('dog', 'brown');
+test.set('elephant', 'gray');
+test.set('frog', 'green');
+test.set('grape', 'purple');
+test.set('hat', 'black');
+test.set('ice cream', 'white');
+test.set('jacket', 'blue');
+test.set('kite', 'pink');
+test.set('lion', 'golden');
